d7981ca3ca4a2f22eb459829f5f2c01a4cbd3acd
laurawilsone <laura.wils204@gmail.com>
2018-05-13 09:51:39 -0500

    fixed game

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,85 +1,179 @@
 
+var game = {
 
-// create the bank of words
-var words = ["lonestar","austin","alamo", "cowboy", "maverick", "dallas"];
-  //var game = {
-  guessed = [];
-  lives = 10;
+    words: {
+        lonestar:{
 
-  function start() {
-    var complete = false;
-    //generate random words
-    var word = words[Math.floor(Math.random() * words.length)];
-     //record number of wins
-    var wins = document.getElementById('wins');
-    // record number of losses
-    var losses = document.getElementById('losses');
-    // list lives left
-    var lives = document.getElementById('lives');
-    // replace blank space with words
-    word.innerHTML = '_'.repeat(word.length);
-   // console.log(wins);
-  }
+        },
 
-  // create a guess function
+        austin: {
 
-  function guess(letter) {
-    if (this.left > 0 && this.complete !== true) {
-      if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
-        this.right(letter);
-      } else {
-        this.wrong(letter);
-        //console.log(letter);
+        },
+
+        alamo: {
+
+        },
+
+        cowboy: {
+
+        },
+
+        maverick: {
+
+        },
+
+        dallas: {
+
+        }
+    }, // word
+
+
+// words
+wordPlay: null,
+letters: [],
+matchedLetters: [],
+guessedLetters: [],
+guessesLeft: 0,
+totalGuesses: 0,
+lettersGuessed: null,
+wins: 0,
+
+
+setupGame: function() {
+
+    var objKeys = Object.keys(this.words);
+    this.wordPlay = objKeys[Math.floor(Math.random() * objKeys.length)];
+    this.letters = this.wordPlay.split("");
+    this.rebuildWordView();
+    this.processUpdateTotalGuesses();
+  },
+
+
+
+ page: function(letter) {
+    if (this.guessesLeft === 0) {
+      this.restartGame();
+    }else {
+      this.updateGuesses(letter);
+      this.newMatchedLetters(letter);
+      this.rebuildWordView();
+
+      if (this.updateWins() === true) {
+        this.restartGame();
       }
     }
-  }
 
-  //a function to show the correct amount of words guessed. 
-  function wins(letter) {
-    for(var i = 0; i < this.word.length; i++) {
-      if (this.word[i] == letter) {
-        var word = wins.innerHTML.split('');
-        word[i] = letter;
-        wins.innerHTML = word.join('');
-      }
+  },
+
+
+  updateGuesses: function(letter) {
+
+    if ((this.guessedLetters.indexOf(letter) === -1) && (this.letters.indexOf(letter) === -1)) {
+      this.guessedLetters.push(letter);
+      this.guessesLeft--;
+
+
+      document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+      document.querySelector("#guessed-letters").innerHTML =
+      this.guessedLetters.join(", ");
     }
-    if (wins.innerHTML.indexOf('_') < 0) {
-      alert('You win!');
-      this.complete = true;
-    }
-  }
-
-  // a function that shows the number of incorrect words guessed. 
-  function losses(letter) {
-    this.guessed.push(letter);
-    this.$wrong.innerHTML += ' ' + letter;
-    this.left--;
-    losses.innerHTML = this.left;
-    if (this.left < 1) {
-      alert('you lose! ' + this.word);
-      this.complete = true;
-    }
-  }
-
-  //};
-
-start();
-
-// creating an onkeyup function to start the game by pressing a key.
-
-document.onkeyup = function(event) {
-  var letter = String.fromCharCode(event.keyCode).toLowerCase();
-  guess(letter);
-
-  // replacing the html 
-  var html = "<p>word</p>" + words 
-  + "<p>letters_already_guessed</p>" 
-
-// pulling the java script into the html
-  document.getElementsByClassName(".game").innerHTML = html;
+  },
 
 
-};
-  
+    processUpdateTotalGuesses: function() {
+
+        this.totalGuesses = this.letters.length + 5;
+        this.guessesLeft = this.totalGuesses;
+    
+        document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+      },
+    
+
+      newMatchedLetters: function(letter) {
+        for (var i = 0; i < this.letters.length; i++) {
+          if ((letter === this.letters[i]) && (this.matchedLetters.indexOf(letter) === -1)) {
+            this.matchedLetters.push(letter);
+          }
+        }
+      },
+    
+
+      rebuildWordView: function() {
+   
+        var wordView = "";
+    
+      
+        for (var i = 0; i < this.letters.length; i++) {
+          if (this.matchedLetters.indexOf(this.letters[i]) !== -1) {
+            wordView += this.letters[i];
+          } else {
+            wordView += "&nbsp;_&nbsp;";
+          }
+        }
+    
+
+        document.querySelector("#current-word").innerHTML = wordView;
+      },
+    
+      restartGame: function() {
+        document.querySelector("#guessed-letters").innerHTML = "";
+        this.wordPlay = null;
+        this.letters = [];
+        this.matchedLetters = [];
+        this.guessedLetters = [];
+        this.guessesLeft = 0;
+        this.totalGuesses = 0;
+        this.letterGuessed = null;
+        this.setupGame();
+        this.rebuildWordView();
+      },
+
+      updateWins: function() {
+        var win;
+    
+        if (this.matchedLetters.length === 0) {
+          win = false;
+        } else {
+          win = true;
+        }
+
+        for (var i = 0; i < this.letters.length; i++) {
+          if (this.matchedLetters.indexOf(this.letters[i]) === -1) {
+            win = false;
+          }
+        }
+    
+
+        if (win) {
+    
+          this.wins = this.wins + 1;
+    
+          document.querySelector("#wins").innerHTML = this.wins;
+    
+          document.querySelector("#music").innerHTML = this.words[this.wordPlay].song +
+          " By " + this.wordPlay;
+    
+          document.querySelector("#band-div").innerHTML =
+            "<img class='band-image' src='assets/images/" +
+            this.words[this.wordPlay].picture + "' alt='" +
+            this.words[this.wordPlay].song + "'>";
+    
+          return true;
+        }
+   
+        return false;
+    }   
+
+}; // game
+
+
+game.setupGame();
+
+    document.onkeyup = function(event) {
+
+    game.letterGuessed = String.fromCharCode(event.which).toLowerCase();
+
+    game.page(game.letterGuessed);
+}
 
 
